4ce0f9433740f97de5c867ba487f572759368768
bpattie <bpattie@uvic.ca>
2017-08-02 16:11:32 -0700

    finished upsampling algorithm.  Simply averages the values of 2 pixels on either side.  Last row and column are duplicated

--- ycbcr2rgb_basic.c
+++ ycbcr2rgb_basic.c
@@ -13,43 +13,27 @@
 #define Y_NORM_FACTOR (256)
 
 //TODO: TEST ME
-char* upsample(char* c_small, unsigned int n_small_rows, unsigned int n_small_cols) {
-  printf("start: upsample\n");
-
-  printf("%d %d\n", n_small_rows, n_small_cols);
-
-  unsigned int n_big_rows = n_small_rows*4;
-  unsigned int n_big_cols = n_small_cols*4;
-
-printf("%d %d\n", n_big_rows, n_big_cols);
-
-  char* c_big = malloc(sizeof(char)*n_big_rows*n_big_cols);
-
+char* upsample(unsigned char* c_small, unsigned int n_small_rows, unsigned int n_small_cols) {
+  unsigned int n_big_rows = n_small_rows << 1;
+  unsigned int n_big_cols = n_small_cols << 1;
+  unsigned char* c_big = malloc(sizeof(char)*n_big_rows*n_big_cols);
   unsigned int row = 0;
   unsigned int col = 0;
-  unsigned int small_i = 0;
-  unsigned int big_i = 0;
+  unsigned int small_i;
+  unsigned int big_i;
 
+  //Pass 1: interpolate all but last 2 rows and last 2 cols.
   while (row < n_small_rows - 1) {
-    printf("row: %d\n", row);
     col = 0;
     while (col < n_small_cols - 1) {
-      printf("    col-%d\n", col);
 
       small_i = row*n_small_cols + col;
       big_i = (row << 1)*n_big_cols + (col << 1);
 
       c_big[big_i]                  = c_small[small_i];
-      printf("        %04x = %04x\n", c_big[big_i], c_small[small_i]);
-
       c_big[big_i + 1]              = (c_small[small_i] + c_small[small_i + 1]) >> 1;
-      printf("        %04x = (%04x + %04x)/2\n", c_big[big_i + 1], c_small[small_i], c_small[small_i + 1]);
-
       c_big[big_i + n_big_cols]     = (c_small[small_i] + c_small[small_i + n_small_cols]) >> 1;
-      printf("        %04x = (%04x + %04x)/2\n", c_big[big_i + n_big_cols], c_small[small_i], c_small[small_i + n_small_cols]);
-
       c_big[big_i + n_big_cols + 1] = (c_small[small_i] + c_small[small_i + n_small_cols + 1]) >> 1;
-      printf("        %04x = (%04x + %04x)/2\n", c_big[big_i + n_big_cols + 1], c_small[small_i], c_small[small_i + n_small_cols + 1]);
 
       col++;
     }
@@ -58,11 +42,49 @@ printf("%d %d\n", n_big_rows, n_big_cols);
     row++;
   }
 
-  printf("end: upsample\n");
+  //Pass 2: interpolate last row, and copy pixes that can't be interpolated
+  row = n_small_rows - 1;
+  col = 0;
+  while (col < n_small_cols - 1) {
+    small_i = row*n_small_cols + col;
+    big_i   = (row << 1)*n_big_cols + (col << 1);
+
+    c_big[big_i]                  = c_small[small_i];
+    c_big[big_i + 1]              = (c_small[small_i] + c_small[small_i + 1]) >> 1;
+    c_big[big_i + n_big_cols]     = c_big[big_i] ;
+    c_big[big_i + n_big_cols + 1] = c_big[big_i + 1];
+
+    col++;
+  }
+
+  //Pass 3: interpolate last column, and copy pixes that can't be interpolated
+  row = 0;
+  col = n_small_cols - 1;
+  while (row < n_small_rows - 1) {
+    small_i = row*n_small_cols + col;
+    big_i   = (row << 1)*n_big_cols + (col << 1);
+
+    c_big[big_i]                  = c_small[small_i];
+    c_big[big_i + 1]              = c_small[small_i];
+    c_big[big_i + n_big_cols]     = (c_small[small_i] + c_small[small_i + n_small_cols]) >> 1;
+    c_big[big_i + n_big_cols + 1] = c_big[big_i + n_big_cols];
+
+    row++;
+  }
+
+  //Pass 4: copy corner values
+  small_i = n_small_rows*n_small_cols - 1;
+  big_i   = (n_big_rows - 1)*(n_big_cols) - 2;
+
+  printf("%d\n", small_i);
+  printf("%d %d %d %d\n", big_i, big_i + 1, big_i + n_big_cols, big_i + n_big_cols + 1);
+
+  c_big[big_i]                  = c_small[small_i];
+  c_big[big_i + 1]              = c_small[small_i];
+  c_big[big_i + n_big_cols]     = c_small[small_i];
+  c_big[big_i + n_big_cols + 1] = c_small[small_i];
+
   return c_big;
-
-  //must deal with edge cases
-
 }
 
 //TODO: TEST ME
@@ -75,10 +97,8 @@ void convert_ycbcr_to_rgb(char * __restrict Y, char * __restrict Cb, char * __re
   unsigned int pixel = 0;
   unsigned int pixel_even = 0;
 
-
-
-
   while (1){}
+
 }
 
 //TODO: convert this to something useful for debugging
@@ -107,8 +127,8 @@ int write_raw_image_data(char* filename, char* data, int size) {
   return 1;
 }
 
-int write_hex_data(char* data, unsigned int n_bytes, char* filename) {
-  printf("Start: write_hex_data\n");
+int write_hex_data(unsigned char* data, unsigned int n_bytes, char* filename) {
+  // printf("Start: write_hex_data\n");
   FILE* fp = fopen(filename, "w");
   fwrite(data, n_bytes, 1, fp);
   fclose(fp);
@@ -116,17 +136,17 @@ int write_hex_data(char* data, unsigned int n_bytes, char* filename) {
 }
 
 //TODO: TEST ME
-char* raw_ycbcr_load(char* filename) {
+unsigned char* raw_ycbcr_load(char* filename) {
   FILE* fp = fopen(filename, "rb");
-  char* data;
+  unsigned char* data;
   long file_size;
-  if(!fp) {
-    // printf("Error opening file (%s) to read\n", filename);
+  if (!fp) {
+    printf("Error opening file (%s) to read\n", filename);
   }
   fseek(fp, 0L, SEEK_END);
   file_size = ftell(fp) + 1;
   rewind(fp);
-  data = (char*)malloc(file_size);
+  data = (unsigned char*)malloc(file_size);
   if(data == NULL) {
     // printf("Error allocating memory\n");
   }
@@ -137,43 +157,53 @@ char* raw_ycbcr_load(char* filename) {
 
 int main( int argc, char** argv) {
   printf("YCbCr to RGB: Simple Conversion\n");
+  if (argc != 3) {
+    printf("Not enough arguments.  Please provide the WIDTH and HEIGHT of the image\n");
+    return 0;
+  }
 
-  // char* y  = raw_ycbcr_load("./input/tiger.bmp");
-  // char* cb = raw_ycbcr_load("")
-  // char* cr = raw_ycbcr_load("")
+  // unsigned char* y  = raw_ycbcr_load("./input/tiger.bmp");
+  // unsigned char* cb = raw_ycbcr_load("")
+  // unsigned char* cr = raw_ycbcr_load("")
 
   //Assumes height and width are positive and less than max signed int value
   unsigned int ncols  = (unsigned int)atoi(argv[1]);
   unsigned int nrows = (unsigned int)atoi(argv[2]);
 
   // printf("allocate for RGB\n");
-  char* rgb = (char*)malloc(3*sizeof(char)*nrows*ncols);
+  unsigned char* rgb = (unsigned char*)malloc(3*sizeof(unsigned char)*nrows*ncols);
 
   //TODO: save info to header
 
   //unsigned int npixels = nrows * ncols;
   // printf("initialize test data\n");
-  char test_y_data[32] = {
+  unsigned char test_y_data[64] = {
+    0xFF,0xEE,0xDD,0xCC,0xBB,0xAA,0x00,0x99,
+    0xFF,0xEE,0xDD,0xCC,0xBB,0xAA,0x00,0x99,
+    0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11,
+    0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11,
     0xFF,0xEE,0xDD,0xCC,0xBB,0xAA,0x00,0x99,
     0xFF,0xEE,0xDD,0xCC,0xBB,0xAA,0x00,0x99,
     0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11,
     0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11
   };
-  char test_cb_data[16] = {
+  unsigned char test_cb_data[16] = {
     0xFF,0xDD,0xBB,0x99,
     0x77,0x55,0x33,0x11,
     0xEE,0xCC,0xAA,0x88,
     0x66,0x44,0x22,0x00
   };
-  char test_cr_data[16] = {
-    0xFF,0xDD,0xBB,0x99,0x77,0x55,0x33,0x11,
-    0xEE,0xCC,0xAA,0x88,0x66,0x44,0x22,0x00
+  unsigned char test_cr_data[16] = {
+    0xFF,0xDD,0xBB,0x99,
+    0x77,0x55,0x33,0x11,
+    0xEE,0xCC,0xAA,0x88,
+    0x66,0x44,0x22,0x00
   };
 
   // printf("allocate for cb_up\n");
   char* cb_up = malloc(sizeof(char)*nrows*ncols);
   cb_up = upsample(test_cb_data, nrows >> 1, ncols >> 1);
-  write_hex_data(cb_up, 32, "upsample/cb_up");
+  write_hex_data(cb_up, 64, "upsample/cb_up");
   // char* cr_up = malloc(sizeof(char)*ncols*nrows);
   // cr_up = upsample(test_cr_data, nrows >> 1, ncols >> 1);
   // write_hex_data(cb_up, 32, "upsample/cr_up");
